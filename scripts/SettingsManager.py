766b5c8f4f0cf3676943ce7c426a1c358b0d9ac8
ChristianLunaSaucedo <christianlunasaucedo@gmail.com>
2026-07-21 20:32:20 -0700

    Fixed Broken Files

--- scripts/SettingsManager.py
+++ scripts/SettingsManager.py
@@ -7,11 +7,6 @@ class SettingsManager(QObject):
 
         self.savedSettings = QSettings("Organization", "App")
 
-        self.savedSettings.setValue("llm_model", llmModel)
-        self.savedSettings.setValue("embedding_model", llmModel)
-        self.savedSettings.setValue("theme", theme)
-        self.savedSettings.setValue("auto_copy", autoCopy)
-
     @Slot(str, str, result=str)
     def FetchSetting(self, settingName, defaultVal):
         return self.savedSettings.value(settingName, defaultVal, str)
